a76e7c0f673eabd23268155819ff480631d3e19f
gogoi23 <75000985+gogoi23@users.noreply.github.com>
2023-05-13 16:17:16 -0700

    Update Wamazon_Sign_In.php

--- wamazon/Wamazon_Sign_In.php
+++ wamazon/Wamazon_Sign_In.php
@@ -1,5 +1,5 @@
 <!-- Author: Anand Gogoi -->
-<!-- This is the tile page. Here the user can see -->
+<!-- This is the login page. The user can log in or make an account. -->
 
 <html>
 <title>
@@ -10,11 +10,13 @@ Wamazon Log In
 
 <H3> Wamazon Log In Page </H3>
 
+<!-- This form takes in the users password and username and redirects thenm to the main.php page-->
 <form action = "main.php" method = "post">
 User Name <input type="text" name="User_Name"><br>
 Password <input type="text" name="Password"> <h6>If you do not have a password you can leave the password blank or type anything. You can change your password when you login. It is recomened that you do so if you do not have one as any one with your user name can log into your account</h6><br>
 <input type="submit" value = "Login">
 <input type = "hidden" name = "Sign_Or_Log" value = "Log" />
+<!--This takes the user to page where they can sign up if they do not have an account. -->
 <a href="signupFrontEnd.php">Click here to make an account</a> 
 </form>
 
